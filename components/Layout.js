85c628e7879e49e5a3fb82d21e6d7310e7de7bc8
Kvazimodo <evdanil@gmail.com>
2023-10-26 18:51:25 +1000

    header, layout, and posts added

---
+++ components/Layout.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import Header from './Header'
+
+function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta
+          name='keywords'
+          content={keywords}
+        />
+        <meta
+          name='description'
+          content={description}
+        />
+        <link
+          rel='icon'
+          href='/favicon.ico'
+        />
+      </Head>
+      <Header />
+      <main className='container mx-auto my-7'>{children}</main>
+    </div>
+  )
+}
+
+export default Layout
+
+Layout.defaultProps = {
+  title: 'Welcome to DevSpace',
+  keywords: 'coding, programmin, devops',
+  description: '',
+}
