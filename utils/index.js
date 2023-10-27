06e5b9792885e978417b29a856680f06614c05da
Kvazimodo <evdanil@gmail.com>
2023-10-27 21:47:45 +1000

    sort fixed finally

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,4 @@
-export const sortByDate = (a, b) => {
+export default function sortByDate(a, b) {
   // console.log(Date.parse(b.frontmatter.date) - Date.parse(a.frontmatter.date))
   // return Date.parse(b.frontmatter.date) - Date.parse(a.frontmatter.date)
   return new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
